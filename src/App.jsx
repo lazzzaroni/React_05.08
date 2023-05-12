73a223be37bdb2c0dc09c4b8686192f7c40c59d3
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-05-12 17:59:37 +0200

    refactor/modify markup

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,16 @@ class App extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState(() => this.initialState);
+  };
+
+  onClick = (e) => {
+    e.preventDefault();
+    this.setState(() => this.initialState);
+  };
+
   render() {
     const {
       name,
@@ -35,86 +45,103 @@ class App extends Component {
       <div className="container">
         <h1 className="heading">Create Profile</h1>
         <form id="form">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            value={name}
-            onChange={this.onChange}
-            name="name"
-            id="name"
-            placeholder="Your Name"
-            autoFocus
-          />
-          <label htmlFor="surname">Surname</label>
-          <input
-            type="text"
-            value={surname}
-            onChange={this.onChange}
-            name="surname"
-            id="surname"
-            placeholder="Your Surname"
-          />
-          <label htmlFor="birthdate">Birthdate</label>
-          <input
-            type="date"
-            value={birthdate}
-            onChange={this.onChange}
-            name="birthdate"
-            id="birthdate"
-          />
-          <label htmlFor="phone">Phone</label>
-          <input
-            type="tel"
-            value={phone}
-            onChange={this.onChange}
-            name="phone"
-            id="phone"
-            placeholder="+1234567890"
-          />
-          <label htmlFor="website">Website</label>
-          <input
-            type="text"
-            value={website}
-            onChange={this.onChange}
-            name="website"
-            id="website"
-            placeholder="your_website.com"
-          />
+          <label className="label">
+            Name
+            <input
+              type="text"
+              value={name}
+              onChange={this.onChange}
+              name="name"
+              placeholder="Your Name"
+              required
+              autoFocus
+            />
+          </label>
+          <label className="label">
+            Surname
+            <input
+              type="text"
+              value={surname}
+              onChange={this.onChange}
+              name="surname"
+              placeholder="Your Surname"
+              // required
+            />
+          </label>
+          <label className="label">
+            Birthdate
+            <input
+              type="date"
+              value={birthdate}
+              onChange={this.onChange}
+              name="birthdate"
+            />
+          </label>
+          <label className="label">
+            Phone
+            <input
+              type="tel"
+              value={phone}
+              onChange={this.onChange}
+              name="phone"
+              placeholder="+1234567890"
+              // required
+            />
+          </label>
+          <label className="label">
+            Website
+            <input
+              type="text"
+              value={website}
+              onChange={this.onChange}
+              name="website"
+              placeholder="https://your_website.com"
+              // required
+            />
+          </label>
 
-          <label htmlFor="about">About</label>
-          <textarea
-            rows="7"
-            value={about}
-            onChange={this.onChange}
-            name="about"
-            id="about"
-            placeholder="Write something about yourself"
-          />
+          <label className="label label_textarea">
+            About
+            <textarea
+              rows="7"
+              value={about}
+              onChange={this.onChange}
+              name="about"
+              placeholder="Write something about yourself"
+              // required
+            />
+            {/* TODO: change message if (X.length > 600) */}
+            <p className="counter">{about.length}/600</p>
+          </label>
 
-          <label htmlFor="techStack">Tech Stack</label>
-          <textarea
-            rows="7"
-            value={techStack}
-            onChange={this.onChange}
-            name="techStack"
-            id="techStack"
-            placeholder="Programming languages, frameworks, tools etc..."
-          />
+          <label className="label label_textarea">
+            Tech Stack
+            <textarea
+              rows="7"
+              value={techStack}
+              onChange={this.onChange}
+              name="techStack"
+              placeholder="Programming languages, frameworks, tools, etc"
+              // required
+            />
+            {/* TODO: change message if (X.length > 600) */}
+            <p className="counter">{techStack.length}/600</p>
+          </label>
 
-          <label htmlFor="lastProject">Last Project</label>
-          <textarea
-            rows="7"
-            value={lastProject}
-            onChange={this.onChange}
-            name="lastProject"
-            id="lastProject"
-            placeholder="Personal blog site, TicTacToe game, etc..."
-          />
+          <label className="label label_textarea">
+            Last Project
+            <textarea
+              rows="7"
+              value={lastProject}
+              onChange={this.onChange}
+              name="lastProject"
+              placeholder="Personal blog site, TicTacToe game, etc"
+            />
+            {/* TODO: change message if (X.length > 600) */}
+            <p className="counter">{lastProject.length}/600</p>
+          </label>
           <div className="buttons">
-            <button
-              className="button_reset"
-              type="reset"
-            >
+            <button className="button_reset" type="reset">
               Cancel
             </button>
             <button className="button_submit" type="submit">
